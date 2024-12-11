47c437f9b9305b4fadc1106128be99ec2b9019c9
wim van ginkel <wim2003vanginkel@gmail.com>
2024-12-11 09:40:16 +0100

    Basic inlog laravel

---
+++ resources/views/Prototype/inlog.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Homepage</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.2.4/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+
+    <input class="border-solid border-black" placeholder="Gebruikersnaam">
+    <input class="" placeholder="wachtwoord">
+    <button>Aanmelden</button>
+    <button>Inloggen</button>
+
+
+</body>
+</html>
