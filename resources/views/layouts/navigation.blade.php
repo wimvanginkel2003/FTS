86b00c2c15bcc3994af1993e2454cd585c0ebacf
wim van ginkel <wim2003vanginkel@gmail.com>
2024-12-14 22:22:43 +0100

    puntenoverzicht klant bijgewerkt prototype

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,9 +4,9 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <div class="shrink-0 flex items-center font-bold text-4xl">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        FTS
                     </a>
                 </div>
 
@@ -98,6 +98,9 @@
             <x-responsive-nav-link :href="route('puntenklant')" :active="request()->routeIs('puntenklant')">
                 {{ __('Punten – klant') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('detailpaginaticketboeken')" :active="request()->routeIs('detailpaginaticketboeken')">
+                {{ __('Detailpagina - klant') }}
+            </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('overzichtbeheerder')" :active="request()->routeIs('overzichtbeheerder')">
                 {{ __('Overzicht - beheerder') }}
             </x-responsive-nav-link>
@@ -107,6 +110,7 @@
             <x-responsive-nav-link :href="route('editbeheerder')" :active="request()->routeIs('editbeheerder')">
                 {{ __('Edit – beheerder') }}
             </x-responsive-nav-link>
+
         </div>
 
         <!-- Responsive Settings Options -->
--- resources/views/puntenklant.blade.php
+++ resources/views/puntenklant.blade.php
@@ -7,40 +7,40 @@
         </p>
     </div>
 
-    <div class="mt-4 border-4 border-black m-2 p-2">
+    <div class="mt-4 m-2 p-6 mb-6 bg-white shadow-md rounded-2xl">
         <div class="flex row justify-between">
         <h1 class="mb-2">Koude drank </h1>
         <h1>Waar:&nbsp;BP</h1>
         </div>
         <p class="mb-2">Benodigde punten: 75</p>
-        <a href="boeken" class="bg-green-700 p-2">Verzilveren</a>
+        <a href="boeken" class="bg-green-700 p-2 rounded-xl">Verzilveren</a>
     </div>
 
-    <div class="mt-4 border-4 border-black m-2 p-2">
+    <div class="mt-4 m-2 p-6 mb-6 bg-white shadow-md rounded-2xl">
         <div class="flex row justify-between">
             <h1 class="mb-2">Chocolate chip cookie </h1>
             <h1>Waar:&nbsp;Starbucks</h1>
         </div>
         <p class="mb-2">Benodigde punten: 100</p>
-        <a href="boeken" class="bg-red-600 p-2">Verzilveren</a>
+        <a href="boeken" class="bg-red-600 p-2 rounded-xl">Verzilveren</a>
     </div>
 
-    <div class="mt-4 border-4 border-black m-2 p-2">
+    <div class="mt-4 m-2 p-6 mb-6 bg-white shadow-md rounded-2xl">
         <div class="flex row justify-between">
             <h1 class="mb-2">Classic Subs</h1>
             <h1>Waar:&nbsp;Subway</h1>
         </div>
         <p class="mb-2">Benodigde punten: 135</p>
-        <a href="boeken" class="bg-red-600 p-2">Verzilveren</a>
+        <a href="boeken" class="bg-red-600 p-2 rounded-xl">Verzilveren</a>
     </div>
 
-    <div class="mt-4 border-4 border-black m-2 p-2">
+    <div class="mt-4 m-2 p-6 mb-6 bg-white shadow-md rounded-2xl">
         <div class="flex row justify-between">
             <h1 class="mb-2">Big Mac menu</h1>
             <h1>Waar:&nbsp;McDonald's</h1>
         </div>
         <p class="mb-2">Benodigde punten: 200</p>
-        <a href="boeken" class="bg-red-600 p-2">Verzilveren</a>
+        <a href="boeken" class="bg-red-600 p-2 rounded-xl">Verzilveren</a>
     </div>
 
 
