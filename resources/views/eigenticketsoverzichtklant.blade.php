244c1f649f60199edf016f660911ddd098b69d69
wim van ginkel <wim2003vanginkel@gmail.com>
2024-12-14 22:06:27 +0100

    eigenticket prototype geperfectioneerd

--- resources/views/eigenticketsoverzichtklant.blade.php
+++ resources/views/eigenticketsoverzichtklant.blade.php
@@ -7,27 +7,54 @@
         <h1>Je geboekte busreisen</h1>
     </div>
 
-    <div class="mt-4 border-4 border-black m-2 p-2">
+    <div class="mt-4 m-2 p-6 mb-6 bg-white shadow-md rounded-2xl ">
 
-        <div class="flex justify-center font-bold">
-            <h2>Utrecht, Nederland &nbsp;&nbsp; -> &nbsp;&nbsp; We Love Green, Parijs </h2>
+        <div class="flex justify-between flex-col">
+
+            <div class="flex justify-row justify-center">
+                <h2 class="font-bold text-2xl"> Utrecht, Nederland</h2>
+            </div>
+
+            <div class="flex items-center justify-between w-full">
+                <!-- Links: 16-03-2025 -->
+                <div class="flex flex-col items-center">
+                    <h2 class="text-xs mb-1">16-03-2025</h2>
+                    <h2 class="text-xs">04:00  <i class="fa-solid fa-bus"></i>  09:00</h2>
+                </div>
+
+                <!-- Cirkeltje met pijlen -->
+                <div class="flex items-center justify-center relative w-20 h-20">
+                    <i class="fa-regular fa-circle text-black text-6xl absolute"></i>
+                    <div class="flex items-center justify-center">
+                        <i class="fa-solid fa-arrow-down text-black mx-1"></i>
+                        <i class="fa-solid fa-arrow-up text-black mx-1"></i>
+                    </div>
+                </div>
+
+                <!-- Rechts: 12-03-2025 -->
+                <div class="flex flex-col items-center">
+                    <h2 class="text-xs mb-1">20-03-2025</h2>
+                    <h2 class="text-xs">10:00 <i class="fa-solid fa-bus"></i> 15:00</h2>
+                </div>
+            </div>
+
+
+
+
+            <div class="flex justify-row justify-center">
+                <h2 class="font-bold text-2xl"> We Love Green, Parijs</h2>
+            </div>
+
         </div>
 
-        <div class=" mt-4">
-            <h3 class="font-bold">Heenreis</h3>
-            Locatie: Utrecht centraal jaarbeurszijde <br>
-            Vertrekijd: 17:00 <br>
-            Aankomstijd: 1:00
+        <div class="flex justify-between">
+            <h2 class="mt-6">€55,00</h2>
+            <x-nav-link @class("mb-2 mt-6") :href="route('detailpaginaticketboeken')" :active="request()->routeIs('detailpaginaticketboeken')">
+                {{ __('Meer informatie') }}
+            </x-nav-link>
+
         </div>
 
-        <div class=" mt-4">
-            <h3 class="font-bold">Terugreis</h3>
-            Locatie: We Love Green, Parijs <br>
-            Vertrekijd: 17:00 <br>
-            Aankomstijd: 1:00
-        </div>
-
-        <h2 class="font-bold mt-2">Prijs:&nbsp; €55,00</h2>
 
     </div>
 </x-app-layout>
