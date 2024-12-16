ef66b5c1ba58959aa400c0dea331b45169fa93e7
wim van ginkel <wim2003vanginkel@gmail.com>
2024-12-16 16:09:47 +0100

    overzicht klanten prototype geupdate

--- resources/views/overzichtbeheer.blade.php
+++ resources/views/overzichtbeheer.blade.php
@@ -1,51 +1,113 @@
 <x-app-layout>
 
-    <div class="mt-4 mb-6">
-        <x-nav-link  :href="route('creëerbeheerder')" :active="request()->routeIs('creëerbeheerder')">
-            {{ __('Create') }}
+    <div class="font-bold m-4 mt-2  sm:w-2/3 flex justify-end">
+        <x-nav-link class="mt-4 sm:text-4xl text-red-600"  :href="route('creëerbeheerder')" :active="request()->routeIs('creëerbeheerder')">
+            {{ __('Create page') }}
         </x-nav-link>
     </div>
 
-    <div class="font-bold m-2 mt-8">
-        <h1>De te boeken busreisen</h1>
-
-    </div>
-
-    <div class="mt-4 border-4 border-black m-2 p-2">
-
-        <div class="flex justify-center font-bold">
-            <h2>Utrecht, Nederland &nbsp; -> &nbsp; We Love Green </h2>
-        </div>
-
-
-        {{--        <div class="mb-2 mt-8">--}}
-        {{--        <a href="boeken" class="border-black border-2 p-2">Boeken</a>--}}
-        {{--        </div>--}}
-
-        <div class="flex justify-end">
-        <x-nav-link  :href="route('editbeheerder')" :active="request()->routeIs('editbeheerder')">
-            {{ __('Edit') }}
-        </x-nav-link>
-        </div>
-
+    <div class="font-bold m-4 mt-2 text-indigo-900 sm:w-2/3  sm:text-4xl">
+        <h1>Overzicht geplande busreizen</h1>
     </div>
 
 
-    <div class="mt-4 border-4 border-black m-2 p-2">
+    <div class="mt-4 m-4 p-6 mb-6 bg-white shadow-md rounded-2xl text-indigo-900 sm:w-2/3 ">
 
-        <div class="flex justify-center font-bold">
-            <h2>Berlijn &nbsp; -> &nbsp; Field Day</h2>
+        <div class="flex justify-between flex-col">
+
+            <div class="flex justify-row justify-center">
+                <h2 class="font-bold text-2xl text-indigo-900"> Utrecht, Nederland</h2>
+            </div>
+
+            <div class="flex items-center justify-between w-full">
+                <!-- Links: 16-03-2025 -->
+                <div class="flex flex-col items-center">
+                    <h2 class="text-xs mb-1 text-indigo-900 md:text-xl">16-03-2025</h2>
+                    <h2 class="text-xs text-indigo-900 md:text-xl">04:00  <i class="fa-solid fa-bus md:text-xl"></i>  09:00</h2>
+                </div>
+
+                <!-- Cirkeltje met pijlen -->
+                <div class="flex items-center justify-center relative w-20 h-20">
+                    <i class="fa-regular fa-circle text-indigo-900 text-6xl absolute"></i>
+                    <div class="flex items-center justify-center">
+                        <i class="fa-solid fa-arrow-down text-indigo-900 mx-1"></i>
+                        <i class="fa-solid fa-arrow-up text-indigo-900 mx-1"></i>
+                    </div>
+                </div>
+
+                <!-- Rechts: 12-03-2025 -->
+                <div class="flex flex-col items-center">
+                    <h2 class="text-xs mb-1 text-indigo-900 md:text-xl">20-03-2025</h2>
+                    <h2 class="text-xs text-indigo-900 md:text-xl">10:00 <i class="fa-solid fa-bus md:text-xl"></i> 15:00</h2>
+                </div>
+            </div>
+
+
+
+
+            <div class="flex justify-row justify-center">
+                <h2 class="font-bold text-2xl text-indigo-900"> We Love Green, Parijs</h2>
+            </div>
+
         </div>
 
-
-        {{--        <div class="mb-2 mt-8">--}}
-        {{--        <a href="boeken" class="border-black border-2 p-2">Boeken</a>--}}
-        {{--        </div>--}}
-
-        <div class="flex justify-end">
-            <x-nav-link  :href="route('editbeheerder')" :active="request()->routeIs('editbeheerder')">
+        <div class="flex justify-between">
+            <h2 class="mt-6 text-indigo-900 font-bold">€55,00</h2>
+            <x-nav-link @class("mb-2 mt-6") :href="route('editbeheerder')" :active="request()->routeIs('editbeheerder')">
                 {{ __('Edit') }}
             </x-nav-link>
+
+        </div>
+
+
+    </div>
+
+    <div class="mt-4 m-4 p-6 mb-6 bg-white shadow-md rounded-2xl sm:w-2/3">
+
+        <div class="flex justify-between flex-col">
+
+            <div class="flex justify-row justify-center">
+                <h2 class="font-bold text-2xl text-indigo-900"> Berlijn, Duitsland</h2>
+            </div>
+
+            <div class="flex items-center justify-between w-full">
+                <!-- Links: 16-03-2025 -->
+                <div class="flex flex-col items-center ">
+                    <h2 class="text-xs mb-1 text-indigo-900 md:text-xl">04-06-2025</h2>
+                    <h2 class="text-xs text-indigo-900 md:text-xl">04:00  <i class="fa-solid fa-bus text-indigo-900 md:text-xl"></i>  19:00</h2>
+                </div>
+
+                <!-- Cirkeltje met pijlen -->
+                <div class="flex items-center justify-center relative w-20 h-20">
+                    <i class="fa-regular fa-circle text-indigo-900 text-6xl absolute"></i>
+                    <div class="flex items-center justify-center">
+                        <i class="fa-solid fa-arrow-down text-indigo-900 mx-1"></i>
+                        <i class="fa-solid fa-arrow-up text-indigo-900 mx-1"></i>
+                    </div>
+                </div>
+
+                <!-- Rechts: 12-03-2025 -->
+                <div class="flex flex-col items-center">
+                    <h2 class="text-xs mb-1 text-indigo-900 md:text-xl">09-06-2025</h2>
+                    <h2 class="text-xs text-indigo-900 md:text-xl">05:00 <i class="fa-solid fa-bus text-indigo-900 md:text-xl"></i> 22:00</h2>
+                </div>
+            </div>
+
+
+
+
+            <div class="flex justify-row justify-center">
+                <h2 class="font-bold text-2xl text-indigo-900"> Field Day, Londen</h2>
+            </div>
+
+        </div>
+
+        <div class="flex justify-between">
+            <h2 class="mt-6 text-indigo-900 font-bold">€70,00</h2>
+            <x-nav-link @class("mb-2 mt-6") :href="route('editbeheerder')" :active="request()->routeIs('editbeheerder')">
+                {{ __('Edit') }}
+            </x-nav-link>
+
         </div>
 
     </div>
